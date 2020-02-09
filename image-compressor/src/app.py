e85a5f2449e30eaef3912bf06731dd92a3276691
Szymon Miks <szymon.miks@netguru.com>
2020-02-09 01:08:49 +0100

    [trello-23]: Added logic to the lambda_handler function
    - imported EventParser
    - imported TinifyApiService
    - added checking if event is s3_put and if it not invoked by lambda itself

--- image-compressor/src/app.py
+++ image-compressor/src/app.py
@@ -1,5 +1,7 @@
 import logging
+
 from parsers.event_parser import EventParser
+from services.tinify_api_service import TinifyApiService
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -9,6 +11,14 @@ def lambda_handler(event, context):
     logger.info(f'Start event handling event={event}')
 
     event_parser = EventParser(event)
-    print(event_parser.get_bucket_name())
 
-    return "ok"
+    if event_parser.is_s3_put_event() and not event_parser.is_invoked_by_lambda():
+        tinyfy_api = TinifyApiService(event_parser)
+
+        result = tinyfy_api.compress_and_resize()
+        logger.info(f'Result from tinyfy_api.resize_and_compress() = {result}')
+
+        return result
+
+    logger.info('Event is NOT S3 Put Object Event')
+    return False
