d67ac17d83dc5d96ab86b05dc606ec9c0a17bab2
Szymon Miks <szymon.miks@netguru.com>
2020-02-09 09:42:19 +0100

    [trello-23]: Added checking content-type to S3Service

--- image-compressor/src/services/s3_service.py
+++ image-compressor/src/services/s3_service.py
@@ -11,12 +11,24 @@ logger.setLevel(logging.INFO)
 
 class S3Service:
     @staticmethod
-    def upload_file(file, object_key):
+    def upload_file(file: bytes, object_key: str) -> None:
         logging.info('Start uploading file to S3 bucket')
 
         try:
             s3 = boto3.client('s3')
-            s3.put_object(Bucket=BUCKET_NAME, Key=object_key, Body=file)
+            s3.put_object(
+                Bucket=BUCKET_NAME, Key=object_key, Body=file, ContentType=S3Service.get_content_type(object_key)
+            )
         except ClientError as e:
             logger.exception(f'Something went wrong during "upload_file", {e}')
             raise
+
+    @staticmethod
+    def get_content_type(object_key: str) -> str:
+        if object_key.lower().endswith('jpg') or object_key.lower().endswith('jpeg'):
+            return 'image/jpeg'
+
+        if object_key.lower().endswith('png'):
+            return 'image/png'
+
+        return 'image/jpeg'
