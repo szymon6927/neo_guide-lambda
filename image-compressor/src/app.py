fef875ebcac66ac215fef77fc958d1b5f1b8185b
Szymon Miks <szymon.miks@netguru.com>
2020-02-09 09:42:52 +0100

    [trello-23]: Added checking file extension in lambda_handler

--- image-compressor/src/app.py
+++ image-compressor/src/app.py
@@ -1,5 +1,6 @@
 import logging
 
+from core.validators import is_valid_file_extension
 from parsers.event_parser import EventParser
 from services.tinify_api_service import TinifyApiService
 
@@ -12,6 +13,10 @@ def lambda_handler(event, context):
 
     event_parser = EventParser(event)
 
+    if not is_valid_file_extension(event_parser.get_object_key()):
+        logger.info('Wrong extension!')
+        return False
+
     if event_parser.is_s3_put_event() and not event_parser.is_invoked_by_lambda():
         tinyfy_api = TinifyApiService(event_parser)
 
@@ -20,5 +25,5 @@ def lambda_handler(event, context):
 
         return result
 
-    logger.info('Event is NOT S3 Put Object Event')
+    logger.info('Event is NOT S3 Put Event or Event was invoked by lambda itself')
     return False
